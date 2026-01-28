85b69c5435a1969322c8541c79ee9cfb25737e7c
Kurt Sanor <sanorkurtkelly@gmail.com>
2026-01-28 17:10:30 +0800

    data handling

--- posts/models.py
+++ posts/models.py
@@ -9,3 +9,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.content[:50]
+    
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on Post {self.post.id}"
